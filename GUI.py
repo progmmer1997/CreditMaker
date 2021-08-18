61f3e0484dfa3f5e9c353d748fc326110ca0e7e4
Анастасия <a.fevraleva@maii.ru>
2021-08-18 13:24:41 +0200

    First GUI creation

---
+++ GUI.py
@@ -0,0 +1,5 @@
+from tkinter import*
+# First GUI creation
+window = Tk()  # Create object  of class TK
+window.title("My first GUI on Python")   # Calling class method title for window object, which give title of our window
+window.mainloop()
