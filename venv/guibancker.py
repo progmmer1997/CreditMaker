55cf0f661964875d703c9819ac0190ce7005cf91
Анастасия <a.fevraleva@maii.ru>
2021-08-22 22:50:17 +0200

    create name_label

--- venv/guibancker.py
+++ venv/guibancker.py
@@ -9,7 +9,21 @@ from tkinter import messagebox
 
 #create window
 window=Tk()
-window.title('GUI bancker')
-window.grid()
-window.geometry('800x400')
+window.title('GUI bancker')  # window title
+window.grid()  # make window visible
+window.geometry('800*800')  # window size
 
+# create name label and entry
+
+name_label=Label(window,text="Имя",font=("Arial",25))
+name_label.grid(column=0,row=0)
+
+
+
+
+
+
+
+
+
+window.mainloop()
