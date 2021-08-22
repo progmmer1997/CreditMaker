c61de19ce6bba8ac045fa242048f674f751f03d1
Анастасия <a.fevraleva@maii.ru>
2021-08-22 22:23:30 +0200

    create window

--- GUI.py
+++ GUI.py
@@ -1,5 +1,11 @@
-from tkinter import*
-# First GUI creation
-window = Tk()  # Create object  of class TK
-window.title("My first GUI on Python")   # Calling class method title for window object, which give title of our window
+from tkinter import *
+from tkinter.ttk import Combobox
+
+window = Tk()
+window.title("Добро пожаловать в приложение PythonRu")
+window.geometry('400x250')
+combo = Combobox(window)
+combo['values'] = (1, 2, 3, 4, 5, "Текст")
+combo.current(1)  # установите вариант по умолчанию
+combo.grid(column=0, row=0)
 window.mainloop()
